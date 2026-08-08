931de200435d162070159c8c222dc6fac8425496
Kshitiz Adhikari <adhikarikshitiz12@gmail.com>
2026-08-09 00:08:39 +0545

    add gin and start server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"go-user-auth/internal/config"
 	"go-user-auth/internal/database"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -12,5 +15,16 @@ func main() {
 	_, err := database.Connect(config)
 	if err != nil {
 		log.Fatal("Failed to connect to the database:", err)
+	}
+
+	router := gin.Default()
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+
+	log.Printf("🚀 Server running on http://localhost:%s", config.AppPort)
+
+	if err := router.Run(":" + config.AppPort); err != nil {
+		log.Fatal("Failed to start server: ", err)
 	}
 }
